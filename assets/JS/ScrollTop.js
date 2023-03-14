c9fff90ed05e5cbaa092afcf234599056b6e9fec
Wolf-Creation <chebaanewajdi91@gmail.com>
2023-03-14 15:10:51 +0100

    first version

--- assets/JS/ScrollTop.js
+++ assets/JS/ScrollTop.js
@@ -1,10 +1,13 @@
 //scroll button function
 var scrollTop = document.getElementById("scroll-top-btn")
+var navScroll = document.querySelector('.navbar-scroll')
 window.addEventListener("scroll", function () {
     if (window.scrollY > 100) {
         scrollTop.style.display = "block";
+        navScroll.style.display= "block";
     } else {
         scrollTop.style.display = "none";
+        navScroll.style.display = "none";
     }
 });
 scrollTop.addEventListener("click", function () {
